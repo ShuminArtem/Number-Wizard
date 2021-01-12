a8cb0d0868dbdacfcea03152805dbf5f1d4d2aa4
ShuminArtem <77163623+ShuminArtem@users.noreply.github.com>
2021-01-12 17:15:56 +0200

    Finish

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentceneIndex + 1);
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
